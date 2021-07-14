171622ce69785098ff98b6a80f842ae6fb7a988e
junquan_yu <junquan_yu@apple.com>
2021-07-14 13:50:19 -0700

    new one3

--- src/main/java/onlineShop/controller/yes.java
+++ src/main/java/onlineShop/controller/yes.java
@@ -3,6 +3,10 @@ package onlineShop.controller;
 public class yes {
     public static void main(String[] args) {
         int y = 2;
-        System.out.println("ni hao");
+
+
+
+
+        System.out.println("what");
     }
 }
